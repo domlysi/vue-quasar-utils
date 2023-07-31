d27078a65f723aed8542f0014e0ae699bfc016d3
Dominik Lysiak <dominik.lysiak@inquant.de>
2023-07-31 08:56:57 +0200

    clean up / vue recommendations

--- src/components/cookieConsent/CookieConsent.vue
+++ src/components/cookieConsent/CookieConsent.vue
@@ -3,34 +3,55 @@
     :model-value="modelValue"
     full-width
     position="bottom"
-    seamless square
-    @update:modelValue="val => $emit('update:modelValue', val)"
+    seamless
+    square
+    @update:model-value="(val) => $emit('update:modelValue', val)"
   >
     <q-card dark>
       <q-card-section class="row items-center no-wrap">
         <slot>
           <div class="row full-width q-col-gutter-md" data-nosnippet="true">
-            <div class="col-12">
-
-            </div>
+            <div class="col-12"></div>
 
             <div class="col-12">
               <div class="text-weight-bold">{{ title }}</div>
               <slot>
-                <div class="text-grey full-width" style="white-space: pre-line;" v-html="text"></div>
+                <div
+                  class="text-grey full-width"
+                  style="white-space: pre-line"
+                  v-html="text"
+                ></div>
               </slot>
             </div>
             <div class="col-12">
               <slot name="actions">
-                <div class="col-shrink q-gutter-md flex items-center justify-center">
+                <div
+                  class="col-shrink q-gutter-md flex items-center justify-center"
+                >
                   <div>
-                    <q-btn :label="btnSettingLabel" color="primary" dense flat @click="settingsModal"/>
+                    <q-btn
+                      :label="btnSettingLabel"
+                      color="primary"
+                      dense
+                      flat
+                      @click="settingsModal"
+                    />
                   </div>
                   <div>
-                    <q-btn :label="btnDenyLabel" color="primary" outline @click="deny"/>
+                    <q-btn
+                      :label="btnDenyLabel"
+                      color="primary"
+                      outline
+                      @click="deny"
+                    />
                   </div>
                   <div>
-                    <q-btn :label="btnAcceptLabel" color="primary" unelevated @click="accept"/>
+                    <q-btn
+                      :label="btnAcceptLabel"
+                      color="primary"
+                      unelevated
+                      @click="accept"
+                    />
                   </div>
                 </div>
               </slot>
@@ -46,109 +67,127 @@
       <q-card-section>
         <q-list>
           <q-item v-for="item in cookieScripts" :key="item.name">
-
             <q-item-section>
               <q-item-label>{{ item.name }} {{ item.label }}</q-item-label>
-              <q-item-label v-if="item.description" caption>{{ item.description }}</q-item-label>
+              <q-item-label v-if="item.description" caption>{{
+                  item.description
+                }}
+              </q-item-label>
             </q-item-section>
 
             <q-item-section side top>
-              <q-toggle :model-value="!!cookieStatus[item.slug]" @update:modelValue="cookieToggle(item)"></q-toggle>
+              <q-toggle
+                :model-value="!!cookieStatus[item.slug]"
+                @update:model-value="cookieToggle(item)"
+              ></q-toggle>
             </q-item-section>
           </q-item>
         </q-list>
       </q-card-section>
       <q-card-actions align="right">
         <q-btn v-close-popup :label="btnCancelLabel" flat></q-btn>
-        <q-btn v-close-popup :label="btnSaveLabel" color="primary" outline @click="save"></q-btn>
+        <q-btn
+          v-close-popup
+          :label="btnSaveLabel"
+          color="primary"
+          outline
+          @click="save"
+        ></q-btn>
       </q-card-actions>
     </q-card>
   </q-dialog>
 </template>
 
 <script>
-import {onMounted, reactive, ref, watch} from 'vue';
-import {Cookies} from 'quasar';
-import {cookieConsent} from './CookieConsent.js'
+import {onMounted, reactive, ref, watch} from "vue";
+import {Cookies} from "quasar";
+import {cookieConsent} from "./CookieConsent.js";
 
 export default {
-  name: 'CookieConsent',
+  name: "CookieConsent",
   props: {
     title: {required: false},
     text: {required: false},
-    btnAcceptLabel: {required: false, default: 'Accept all'},
-    btnDenyLabel: {required: false, default: 'Only necessary'},
-    btnSettingLabel: {required: false, default: 'Settings'},
-    btnSaveLabel: {required: false, default: 'Save'},
-    btnCancelLabel: {required: false, default: 'Cancel'},
+    btnAcceptLabel: {required: false, default: "Accept all"},
+    btnDenyLabel: {required: false, default: "Only necessary"},
+    btnSettingLabel: {required: false, default: "Settings"},
+    btnSaveLabel: {required: false, default: "Save"},
+    btnCancelLabel: {required: false, default: "Cancel"},
     modelValue: {default: false, required: true},
-    disabled: {default: false}
+    disabled: {default: false},
   },
-  emits: ['update:modelValue', 'accepted', 'acceptedBtnClick', 'deny', 'denyBtnClick', 'close'],
+  emits: [
+    "update:modelValue",
+    "accepted",
+    "acceptedBtnClick",
+    "deny",
+    "denyBtnClick",
+    "close",
+  ],
   setup(props, {emit}) {
-    const showSettings = ref(false)
-    const cookieScripts = ref([])
-    const cookieStatus = reactive({})
+    const showSettings = ref(false);
+    const cookieScripts = ref([]);
+    const cookieStatus = reactive({});
 
-    if (!Cookies.get('cookieConsentDialog')) {
-      if (props.disabled === true) return
-      emit('update:modelValue', true)
+    if (!Cookies.get("cookieConsentDialog")) {
+      if (props.disabled === true) return;
+      emit("update:modelValue", true);
     }
 
     const isCookieActives = function (itemSlug) {
-      return !!Cookies.get(itemSlug)
-    }
+      return !!Cookies.get(itemSlug);
+    };
 
     watch(
       () => cookieScripts,
       (val) => {
-        val.value.forEach(item => {
-          cookieStatus[item.slug] = item.isActive()
-        })
+        val.value.forEach((item) => {
+          cookieStatus[item.slug] = item.isActive();
+        });
       },
       {deep: true}
-    )
+    );
 
     const cookieToggle = function (item) {
       if (Cookies.get(item.slug, true)) {
-        item.removeCookie()
-        cookieStatus[item.slug] = false
+        item.removeCookie();
+        cookieStatus[item.slug] = false;
       } else {
-        item.setCookie(true)
-        cookieStatus[item.slug] = true
+        item.setCookie(true);
+        cookieStatus[item.slug] = true;
       }
-    }
+    };
 
     const accept = function () {
-      cookieConsent.accept({all: true})
-      emit('update:modelValue', false)
-      emit('accepted')
-      emit('acceptedBtnClick')
-      emit('close')
-    }
+      cookieConsent.accept({all: true});
+      emit("update:modelValue", false);
+      emit("accepted");
+      emit("acceptedBtnClick");
+      emit("close");
+    };
 
     const deny = function () {
-      cookieConsent.removeNotRequiredCookies()
-      cookieConsent.accept({all: false})
-      emit('update:modelValue', false)
-      emit('deny')
-      emit('denyBtnClick')
-      emit('close')
-    }
+      cookieConsent.removeNotRequiredCookies();
+      cookieConsent.accept({all: false});
+      emit("update:modelValue", false);
+      emit("deny");
+      emit("denyBtnClick");
+      emit("close");
+    };
 
     const save = function () {
-      cookieConsent.accept({all: false})
-      emit('update:modelValue', false)
-      emit('close')
-    }
+      cookieConsent.accept({all: false});
+      emit("update:modelValue", false);
+      emit("close");
+    };
 
     const settingsModal = function () {
-      showSettings.value = true
-    }
+      showSettings.value = true;
+    };
 
     onMounted(() => {
-      cookieScripts.value = cookieConsent.getAll()
-    })
+      cookieScripts.value = cookieConsent.getAll();
+    });
 
     return {
       accept,
@@ -161,8 +200,8 @@ export default {
       cookieToggle,
       isCookieActives,
       cookieStatus,
-      close
-    }
-  }
-}
+      close,
+    };
+  },
+};
 </script>
--- src/components/form/DynamicInput.vue
+++ src/components/form/DynamicInput.vue
@@ -7,207 +7,214 @@
       :model-value="component.attrs.modelValue"
       v-bind="component.attrs"
       v-on="component.handlers"
-      @update:modelValue="val => {$emit('update:modelValue', val); component.attrs.modelValue = val}"
+      @update:model-value="
+        (val) => {
+          $emit('update:modelValue', val);
+          component.attrs.modelValue = val;
+        }
+      "
     >
       <template v-for="(_, name) in $slots" v-slot:[name]="data">
-        <slot :name="name" v-bind:slot="data"></slot>
+        <slot v-bind:slot="data" :name="name"></slot>
       </template>
     </component>
 
-    <template v-if="component.component?.name === 'QCheckbox' && component.attrs.hint">
+    <template
+        v-if="component.component?.name === 'QCheckbox' && component.attrs.hint"
+    >
       <div class="text-muted text-small">{{ component.attrs.hint }}</div>
     </template>
   </div>
 </template>
 
 <script lang="ts">
-import {QCheckbox, QFile, QInput, QSelect} from 'quasar';
-import {computed, defineComponent, PropType} from 'vue'
-
+import {QCheckbox, QFile, QInput, QSelect} from "quasar";
+import {computed, defineComponent, PropType} from "vue";
 
 export interface FieldOption {
-  value: string,
-  display_name: string
+  value: string;
+  display_name: string;
 }
 
 export interface Field {
-  type: string,
-  required: boolean,
-  read_only: boolean,
-  label: string,
-  children?: Array<Field>
-  choices?: Array<FieldOption>,
-  max_length: number,
-  help_text: string
+  type: string;
+  required: boolean;
+  read_only: boolean;
+  label: string;
+  children?: Array<Field>;
+  choices?: Array<FieldOption>;
+  max_length: number;
+  help_text: string;
 }
 
 const TYPE_MAPPING = {
-  'field': undefined,
-  'boolean': {
+  field: undefined,
+  boolean: {
     component: QCheckbox,
-    attrs: {}
+    attrs: {},
   },
-  'string': {
+  string: {
     component: QInput,
     attrs: {
-      type: 'text'
-    }
+      type: "text",
+    },
   },
-  'url': {
+  url: {
     component: QInput,
     attrs: {
-      type: 'url'
-    }
+      type: "url",
+    },
   },
-  'email': {
+  email: {
     component: QInput,
     attrs: {
-      type: 'email'
-    }
+      type: "email",
+    },
   },
-  'regex': {
-    component: QInput
+  regex: {
+    component: QInput,
   },
-  'slug': {
-    component: QInput
+  slug: {
+    component: QInput,
   },
-  'integer': {
+  integer: {
     component: QInput,
     attrs: {
-      type: 'number'
-    }
+      type: "number",
+    },
   },
-  'float': {
+  float: {
     component: QInput,
     attrs: {
-      type: 'number'
-    }
+      type: "number",
+    },
   },
-  'decimal': {
+  decimal: {
     component: QInput,
     attrs: {
-      type: 'number'
-    }
+      type: "number",
+    },
   },
-  'date': {
+  date: {
     component: QInput,
     attrs: {
-      type: 'date'
-    }
+      type: "date",
+    },
   },
-  'datetime': {
+  datetime: {
     component: QInput,
     attrs: {
-      type: 'date'
-    }
+      type: "date",
+    },
   },
-  'time': {
+  time: {
     component: QInput,
     attrs: {
-      type: 'time'
-    }
+      type: "time",
+    },
   },
-  'choice': {
-    component: QSelect
+  choice: {
+    component: QSelect,
   },
-  'multiple choice': {
-    component: undefined
+  "multiple choice": {
+    component: undefined,
   },
-  'file upload': {
-    component: QFile
+  "file upload": {
+    component: QFile,
   },
-  'image upload': {
-    component: QFile
+  "image upload": {
+    component: QFile,
   },
-  'list': {
-    component: undefined
+  list: {
+    component: undefined,
   },
-  'nested object': {
-    component: undefined
+  "nested object": {
+    component: undefined,
   },
-}
+};
 
 export default defineComponent({
-  name: 'DynamicInput',
+  name: "DynamicInput",
   props: {
     field: {
       required: true,
-      type: Object as PropType<Field>
+      type: Object as PropType<Field>,
     },
     fieldComponent: {
       default: undefined,
-      type: Object
+      type: Object,
     },
     errors: {
       default: undefined,
     },
     modelValue: {
-      required: true
-    }
+      required: true,
+    },
   },
-  emits: ['update:modelValue'],
+  emits: ["update:modelValue"],
   setup(props, ctx) {
     const component = computed(() => {
       if (props.fieldComponent) {
-        return props.fieldComponent
+        return props.fieldComponent;
       }
       if (!props.field?.type) {
-        throw (`no field type given`)
+        throw `no field type given`;
       }
       if (!TYPE_MAPPING[props.field.type]) {
-        throw (`Field "${props.field.type}" has no component in mapping.`)
+        throw `Field "${props.field.type}" has no component in mapping.`;
       }
 
       let r = {
-        component: TYPE_MAPPING[props.field.type]['component'],
+        component: TYPE_MAPPING[props.field.type]["component"],
         attrs: {
-          ...TYPE_MAPPING[props.field.type]['attrs'],
+          ...TYPE_MAPPING[props.field.type]["attrs"],
           label: props.field.label,
           required: props.field.required,
           hint: props.field.help_text,
           readonly: props.field.read_only,
           maxlength: props.field.max_length,
-          behavior: 'menu',
+          behavior: "menu",
           modelValue: props.modelValue,
-          ...ctx.attrs
+          ...ctx.attrs,
         },
-        handlers: {}
-      }
+        handlers: {},
+      };
 
       if (r.attrs.required) {
-        r['attrs']['label'] = r['attrs']['label'] + ' *'
+        r["attrs"]["label"] = r["attrs"]["label"] + " *";
       }
 
-      if (r['component'] === QSelect) {
-        r.attrs['modelValue'] = r.attrs['modelValue'] === undefined ? null : r.attrs['modelValue']
+      if (r["component"] === QSelect) {
+        r.attrs["modelValue"] =
+            r.attrs["modelValue"] === undefined ? null : r.attrs["modelValue"];
         if (props.field.choices) {
-          r.attrs['options'] = props.field.choices.flatMap((obj) => {
+          r.attrs["options"] = props.field.choices.flatMap((obj) => {
             return {
-              label: obj['display_name'],
-              value: obj['value']
-            }
-          })
+              label: obj["display_name"],
+              value: obj["value"],
+            };
+          });
         }
       }
 
-      if (r['component'] === QCheckbox) {
-        r.attrs['class'] = !r.attrs?.class ? 'q-py-sm' : r.attrs.class
-        if (ctx.attrs['no-indeterminate']) {
-          r.attrs['modelValue'] = r.attrs['modelValue'] || false
+      if (r["component"] === QCheckbox) {
+        r.attrs["class"] = !r.attrs?.class ? "q-py-sm" : r.attrs.class;
+        if (ctx.attrs["no-indeterminate"]) {
+          r.attrs["modelValue"] = r.attrs["modelValue"] || false;
         }
       }
 
       r.attrs = {
         ...r.attrs,
-        ...ctx.attrs
-      }
+        ...ctx.attrs,
+      };
 
-      return r
-    })
+      return r;
+    });
 
     return {
       component,
-    }
-  }
-})
+    };
+  },
+});
 </script>
